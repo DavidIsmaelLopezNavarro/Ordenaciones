53e0774877114dbc67e1b8028a763cc48aa266f4
USER <USER@DESKTOP-49VLK0T>
2022-12-09 08:05:34 -0500

    Ejercicios de Ordenaciones

---
+++ src/metodos/TestMetodos.java
@@ -0,0 +1,13 @@
+
+package metodos;
+
+public class TestMetodos {
+
+    public static void main(String[] args) {
+       MetodosDeOrdenamiento ordena = new MetodosDeOrdenamiento();
+       int [] arreglo = {4, 3, 7, -125, 265 , 7864, 215, -1256};
+       ordena.ordenamientoporSeleccion(arreglo);
+       ordena.imprime(arreglo);
+    }
+    
+}
